eea81b254a181a74b78550369ae26519da40487e
Xavier Torres-Casso <xavi.muchado@gmail.com>
2021-05-04 15:15:12 -0700

    Complete mern-infrastructure

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+router.post('/login', usersCtrl.login);
+// POST /api/users
+router.post('/', usersCtrl.create);
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+
+module.exports = router;
