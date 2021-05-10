9faf11d98ab435cd1b2007644361e3118d83e12a
Xavier Torres-Casso <xavi.muchado@gmail.com>
2021-05-10 12:58:56 -0700

    Added crud-help ++

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,3 +1,10 @@
-export default function NewOrderPage() {
-  return <h1>NewOrderPage</h1>
+import { useState, useEffect } from 'react';
+
+export default function NewOrderPage({ user, setUser }) {
+  const [menuItems, setMenuItems] = useState([]);
+
+  return (
+  <h1>NewOrderPage</h1>
+
+  );
 }
