d4278c8cb98bbcb29bbaf8640866ad957f5cc2ff
Xavier Torres-Casso <xavi.muchado@gmail.com>
2021-05-10 15:21:43 -0700

    utilities added

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,9 +2,10 @@ import * as usersAPI from './users-api';
 
 export async function signUp(userData) {
   try {
-    // usersAPI.signUp will resolve to the token sent back
-    // from the server
+    // Delegate the network request code to the users-api.js
+    // service module which will ultimately return a JWT
     const token = await usersAPI.signUp(userData);
+    // Persist the "token"
     localStorage.setItem('token', token);
     return getUser();
   } catch {
@@ -12,15 +13,33 @@ export async function signUp(userData) {
   }
 }
 
+export async function login(credentials) {
+  try {
+    const token = await usersAPI.login(credentials);
+    localStorage.setItem('token', token);
+    return getUser();
+  } catch {
+    throw new Error('Bad Credentials');
+  }
+}
+
+export function logOut() {
+  localStorage.removeItem('token');
+}
+
+export function checkToken() {
+  return usersAPI.checkToken()
+    .then(dateStr => new Date(dateStr));
+}
+
 export function getToken() {
   // getItem returns null if there's no string
   const token = localStorage.getItem('token');
   if (!token) return null;
-  // Obtain the payload of the token
+  // Check if expired, remove if it is
   const payload = JSON.parse(atob(token.split('.')[1]));
   // A JWT's exp is expressed in seconds, not milliseconds, so convert
   if (payload.exp < Date.now() / 1000) {
-    // Token has expired - remove it from localStorage
     localStorage.removeItem('token');
     return null;
   }
@@ -31,25 +50,4 @@ export function getUser() {
   const token = getToken();
   // If there's a token, return the user in the payload, otherwise return null
   return token ? JSON.parse(atob(token.split('.')[1])).user : null;
-}
-
-export function logOut() {
-  localStorage.removeItem('token');
-}
-
-export async function login(credentials) {
-  try {
-    // usersAPI.signUp will resolve to the token sent back
-    // from the server
-    const token = await usersAPI.login(credentials);
-    localStorage.setItem('token', token);
-    return getUser();
-  } catch {
-    throw new Error('Invalid Log In');
-  }
-}
-
-export function checkToken() {
-  return usersAPI.checkToken()
-    .then(dateStr => new Date(dateStr));
 }
