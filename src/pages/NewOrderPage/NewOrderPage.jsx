545e06d50b162fef9e484bdf5b981fbf7e70c4fd
Xavier Torres-Casso <xavi.muchado@gmail.com>
2021-05-10 13:24:30 -0700

    Add Item controller

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -3,8 +3,18 @@ import { useState, useEffect } from 'react';
 export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
 
-  return (
-  <h1>NewOrderPage</h1>
+  useEffect(function() {
+    console.log('useEffect ran smooth');
+  });
 
+  useEffect(function() {
+    console.log('useEffect runs only after first render');
+  }, [menuItems]);
+
+  return (
+    <>
+      <h1>NewOrderPage</h1>
+      <button onClick={() => setMenuItems(Date.now())}>Trigger re-render</button>
+    </>
   );
 }
