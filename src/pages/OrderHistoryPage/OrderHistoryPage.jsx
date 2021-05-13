0de6ac0e06fbb7b2fb072bee74d7d1a087bcd490
Xavier Torres-Casso <xavi.muchado@gmail.com>
2021-05-12 23:15:12 -0700

    Oh boy

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,14 +1,45 @@
-import * as userService from '../../utilities/users-service';
+import { Link } from 'react-router-dom';
+import './OrderHistoryPage.css';
+// import * as usersService from '../../utilities/users-service';
+import * as ordersAPI from '../../utilities/orders-api';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+// import NavBar from '../../components/NavBar/NavBar';
+import OrderList from '../../components/OrderList/OrderList';
+import { useState, useEffect } from 'react';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
 
-export default function OrderHistoryPage() {
-  async function handleCheckToken() {
-    const expDate = await userService.checkToken();
-    console.log(expDate);
-  }
+
+export default function OrderHistoryPage({ user, setUser }) {
+  const [orders, setOrders] = useState([]);
+  const [activeOrder, setActiveOrder] = useState(null);
+
+  useEffect(function() {
+    async function getOrders() {
+      const updatedOrders = await ordersAPI.getAll();
+      setOrders(updatedOrders);
+      setActiveOrder(updatedOrders[0]);
+    }
+    getOrders();
+  }, []);
+
+
   return (
-    <>
-      <h1>OrderHistoryPage</h1>
-      <button onClick={handleCheckToken}>Check When My Login Expires</button>
-    </>  
+    <main className="OrderHistoryPage">
+      <aside>
+        {/* <NavBar user={user} set={setUser}/> */}
+        <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      
+      <div>
+        <OrderList orders={orders} setActiveOrder={setActiveOrder} activeOrder={activeOrder} />
+      </div>
+      <div>
+        <OrderDetail order={activeOrder} />
+      </div>
+
+
+
+    </main>
   );
 }
